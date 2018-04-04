256f1ca9796823cf50bc9c5390b6973b1ece193d
wang-seven <543552638@qq.com>
2018-04-04 15:56:42 +0800

    教师管理 查询

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -1,16 +1,25 @@
 <?php
 namespace app\index\controller;     // 该文件的位于application\index\controller文件夹
 // 导入app\common\model\Teacher模型，并给它重新起个名字：SmallTeacher
-use app\common\model\Teacher;  // 教师模型 带有别名
+use app\common\model\Teacher;
+use think\Controller;  // 教师模型 带有别名
 /**
  * 教师管理
  */
-class TeacherController
+class TeacherController extends Controller
 {
     public function index()
     {
         $Teacher = new Teacher;
-        $teacher = $Teacher ->select();
-        dump($teacher);
+        $teachers = $Teacher->select();
+
+        // 向V层传数据
+        $this->assign('teachers', $teachers);
+
+        // 取回打包后的数据
+        $htmls = $this->fetch();
+
+        // 将数据返回给用户
+        return $htmls;
     }
 }
